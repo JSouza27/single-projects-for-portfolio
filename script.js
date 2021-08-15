d7a9619b5b075247c537f1c4891076bed2389611
Jonathan Souza <souzajonathan570@gmail.com>
2021-08-15 18:19:39 -0300

    function convertBinaryToDecimal completed

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const inputBin = document.querySelector('.input-bin');
 const btnConvert = document.querySelector('.btn-convert');
 const spanError = document.querySelector('.error');
+const spanResult = document.querySelector('.result');
 
 function fail(msg) {
   spanError.innerHTML = msg;
 }
 
+function assignResult(sum) {
+  spanResult.innerHTML = sum;
+}
+
 function convertBinaryToDecimal(input) {
+  let sum = 0;
+
   if (/^[01]+$/.test(input.value)) {
     const valueBinary = input.value.split('');
-    return console.log(valueBinary);
+
+    valueBinary.reverse().forEach((number, index) => {
+      sum += 2 ** index * parseInt(number, 10);
+    });
+
+    return assignResult(sum.toFixed(2));
   }
   
   return fail('Digite um valor binário 0 ou 1');
