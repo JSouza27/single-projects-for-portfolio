52158ee6870de3a578601246bfac385b90f703cc
Jonathan Souza <souzajonathan570@gmail.com>
2021-08-27 03:26:14 -0300

    new styles have been added with Sass

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ const inputBin = document.querySelector('.input-bin');
 const btnConvert = document.querySelector('.btn-convert');
 const spanError = document.querySelector('.error');
 const spanResult = document.querySelector('.result');
+const divInput = document.querySelector('.div-input');
 
-function fail(msg) {
-  spanError.innerHTML = msg;
+function validation(e) {
+  const pattern = /^[01]+$/;
+  const valueInput = e.code.substring(5);
+
+  if (valueInput.match(pattern)) {
+    spanError.classList.add('valid');
+    spanError.classList.remove('invalid');
+
+    divInput.classList.add('input-valid');
+    divInput.classList.remove('input-invalid');
+
+    spanError.innerHTML = 'Valor válido';
+  } else {
+    spanError.classList.remove('valid');
+    spanError.classList.add('invalid');
+
+    divInput.classList.remove('input-valid');
+    divInput.classList.add('input-invalid');
+
+    spanError.innerHTML = 'Valor inválido, digite uma segquência de números com 0 ou 1';
+  }
 }
 
 function assignResult(sum) {
@@ -13,19 +33,17 @@ function assignResult(sum) {
 
 function convertBinaryToDecimal(input) {
   let sum = 0;
-
-  if (/^[01]+$/.test(input.value)) {
-    const valueBinary = input.value.split('');
-
-    valueBinary.reverse().forEach((number, index) => {
-      sum += 2 ** index * parseInt(number, 10);
-    });
-
-    return assignResult(sum.toFixed(2));
-  }
   
-  return fail('Digite um valor binário 0 ou 1');
+  const valueBinary = input.value.split('');
+  
+  valueBinary.reverse().forEach((number, index) => {
+    sum += 2 ** index * parseInt(number, 10);
+  });
+  
+  return assignResult(sum.toFixed(1));
 }
+
+inputBin.addEventListener('keydown', validation);
 
 btnConvert.addEventListener('click', () => {
   convertBinaryToDecimal(inputBin);
